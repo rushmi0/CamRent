7fce993b4b881d27598e243b92bdeceb8d93bcb5
CJ1426 <japanchaiyapat@gmail.com>
2023-09-19 12:55:40 +0700

    move content in profile screen to center

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,6 @@ function App() {
           {/* RootLayout contain navigation bar as a component to use in everypage*/}
           <Route element={<RootLayout />}>
             {/* every elements inside RootLayout will show Navigation Bar on the top of the page */}
-            <Route index element={<Home />}></Route>
-            <Route path="/shop" element={<Shop />}></Route>
           </Route>
           {/* RootLayout-end */}
 
--- src/components/operationcontrol/customer/CustomerProfile.jsx
+++ src/components/operationcontrol/customer/CustomerProfile.jsx
@@ -5,23 +5,23 @@ import { useLocation } from "react-router-dom";
 function CustomerProfile() {
   const location = useLocation().hash;
   return (
-    <>
-      <div className="mt-3 grid grid-cols-1 lg:grid-cols-5">
-        <div className="mb-3 lg:mb-0 lg:ml-3 rounded border">
-          <ul className="mx-3 text-lg">
-            <a href="#">
-              <li className="py-2 border-b-2">Edit profile</li>
-            </a>
-            <a href="#order">
-              <li className="py-2 lg:border-b-2">My order history</li>
-            </a>
-          </ul>
-        </div>
-        <div className="col-span-4 xl:col-span-3 lg:ml-3">
-          {location === "" ? <EditProfile /> : <>order</>}
+      <div className="container mx-auto my-auto mt-[4rem]">
+        <div className="mt-3 grid grid-cols-1 lg:grid-cols-5">
+          <div className="mb-3 lg:mb-0 lg:ml-3 rounded border">
+            <ul className="mx-3 text-lg">
+              <a href="#">
+                <li className="py-2 border-b-2">Edit profile</li>
+              </a>
+              <a href="#order">
+                <li className="py-2 lg:border-b-2">My order history</li>
+              </a>
+            </ul>
+          </div>
+          <div className="col-span-4 lg:ml-3">
+            {location === "" ? <EditProfile /> : <>order</>}
+          </div>
         </div>
       </div>
-    </>
   );
 }
 
