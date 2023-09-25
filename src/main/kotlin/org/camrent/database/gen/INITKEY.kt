9fe87fff02712f2dd655f2dc3e09ef6ed471f6ea
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-09-25 22:45:31 +0700

    update code

--- src/main/kotlin/org/camrent/database/gen/INITKEY.kt
+++ src/main/kotlin/org/camrent/database/gen/INITKEY.kt
@@ -3,15 +3,15 @@ package org.camrent.database.gen
 object INITKEY {
 
     val MAIN: Map<String, String> = mapOf(
-        "PS" to "PS-0001", // PersonID
-        "CTM" to "CTM-0001", // CustomerID
-        "ST" to "ST-0001", // StoreID
-        "ADDR" to "ADDR-0001", // AddressID
-        "SC" to "SC-0001", // ScoreID
-        "OD" to "OD-0001",  // OrderID
-        "TX" to "TX-0001", // TxID
-        "PD" to "PD-0001", // ProductID
-        "FN" to "FN-0001", // FineID
+        "PS" to "PS-0100", // PersonID
+        "CTM" to "CTM-0100", // CustomerID
+        "ST" to "ST-0100", // StoreID
+        "ADDR" to "ADDR-0100", // AddressID
+        "SC" to "SC-0100", // ScoreID
+        "OD" to "OD-0100",  // OrderID
+        "TX" to "TX-0100", // TxID
+        "PD" to "PD-0100", // ProductID
+        "FN" to "FN-0100", // FineID
     )
 
 }
