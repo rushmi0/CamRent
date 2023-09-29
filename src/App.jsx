74addafab8d3c09b1af20783e31e39bb4666b698
MasterAvocado1150 <WorkSP.Git.MinthadA2507@gmail.com>
2023-09-30 01:06:29 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 import RootLayout from "./components/RootLayout";
 import Home from "./page/Home";
 import Shop from "./components/shop/Shop"
+import ProductsDetail from "./components/product/ProductsDetail.jsx";
+import ProductsOverview from "./components/product/ProductsOverview.jsx";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
             {/* every elements inside RootLayout will show Navigation Bar on the top of the page */}
             <Route index element={<Home />}></Route>
             <Route path="/shop" element={<Shop />}></Route>
+            <Route path="/detail" element={<ProductsOverview />}></Route>
 
           </Route>
           {/* RootLayout-end */}
--- src/components/product/ProductsDetail.jsx
+++ src/components/product/ProductsDetail.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-function ProductsDetail() {
-  return (
-    <div>ProductsDetail</div>
-  )
-}
+const ProductDetail = () => {
+    return (
+        <>
+            <div className="mx-auto">
+                <div className="px-4 sm:px-0 pt-4 ml-7">
+                    <h3 className="text-base font-semibold leading-7 text-gray-900">SPECIFICATION</h3>
+                </div>
+                <div className="mt-6 border-t border-gray-100">
+                    <dl className="divide-y divide-gray-100">
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm font-medium leading-6 text-gray-900 text-center">ความละเอียดภาพสูงสุด (Max Resolution):</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 text-center">8256 x 5504</dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm font-medium leading-6 text-gray-900 text-center">สัดส่วนภาพ (Image Ratio):</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 text-center">1:1, 3:2, 4:5, 16:9</dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm font-medium leading-6 text-gray-900 text-center">พิกเซล (Effective Pixels):</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 text-center">45.7 Megapixel</dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm font-medium leading-6 text-gray-900 text-center">เซลล์รับภาพ (Image Sensor):</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 text-center">Full frame (35.9 x 23.9 mm)/CMOS/Expeed 6</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
 
-export default ProductsDetail
+        </>
+    );
+};
+
+export default ProductDetail;
