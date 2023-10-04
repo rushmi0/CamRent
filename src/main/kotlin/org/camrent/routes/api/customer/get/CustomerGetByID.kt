0eae62397f84d093e205f50e3b28cd92973a8d42
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-10-04 19:38:37 +0700

    เพิ่มการตรวจจับ ปัญหาที่อาจเกิดขึ้นได้

--- src/main/kotlin/org/camrent/routes/api/customer/get/CustomerGetByID.kt
+++ src/main/kotlin/org/camrent/routes/api/customer/get/CustomerGetByID.kt
@@ -2,45 +2,84 @@ package org.camrent.routes.api.customer.get
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.camrent.database.service.CustomerService
-
+import org.camrent.security.securekey.ECDSA
+import org.camrent.security.securekey.EllipticCurve.getDecompress
+import org.camrent.security.securekey.Sha256
+import org.camrent.utils.ShiftTo.B32decode
+import org.camrent.utils.ShiftTo.ByteArrayToBigInteger
 
 fun Route.CustomerGetByID() {
 
     get("customers/id/{id}") {
-        // อ่านค่า IP address และ port ของ client จาก request
-        val clientIp = call.request.local.remoteHost
-        val clientPort = call.request.local.port
-        // อ่านค่า cookie ชื่อ "user" จาก request
-        val userCookie = call.request.cookies["user"]
-        println("Client IP: $clientIp, Port: $clientPort Cookie: $userCookie")
 
-        // ดึงค่า Customer name จากพารามิเตอร์ที่ระบุใน URL และแปลงเป็น Int หรือ null ถ้าไม่สามารถแปลงได้
-        val id = call.parameters["id"]?.toIntOrNull()
+        try {
+            // ดึงค่า Customer name จากพารามิเตอร์ที่ระบุใน URL และแปลงเป็น Int หรือ null ถ้าไม่สามารถแปลงได้
+            val id = call.parameters["id"]?.toIntOrNull() ?: throw IllegalArgumentException("`id` ไม่ถูกต้องหรือไม่ได้ระบุ")
 
-        if (id != null) {
             // ค้นหาข้อมูลลูกค้าด้วย ID
-            val customer = CustomerService.findCustomerByUserID(id)
+            val customer = CustomerService.findCustomerByUserID(id) ?: throw NotFoundException("ไม่พบลูกค้าสำหรับ `ID`: $id")
 
-            if (customer != null) {
+            // ดึงค่า Witness และ Signature จาก Headers
+            val signature = call.request.headers["Signature"]
+            val witness = call.request.headers["Witness"]
+
+            // ตรวจสอบว่า `Signature` และ `Witness` ไม่เป็น `null`
+            if (signature == null || witness == null) {
+                throw IllegalArgumentException("`Signature` หรือ `Witness` เป็น `null`")
+            }
+
+            val rawPermit = Sha256.hash(witness.toByteArray()).ByteArrayToBigInteger()
+
+            // ถอดรหัส (decode) เพื่อดึงเอาพิกัด R และ S ออกมาใช้ในการคำนวณ
+            val signatureRecovered = ECDSA.derRecovered(signature)!!
+
+            // ดึง `AuthKey` ของ `User` จากฐานข้อมูล และถอดรหัสด้วย Bech32
+            // เพื่อให้ได้ Public Key (ECC) แบบฐาน 16
+            val publicKey = customer.authKey?.B32decode()?.third!!
+
+            // นำ Public Key ที่ถูกบีบอัดให้เป็นตัวเลขฐาน 10 มาคำนวณหา Public Key
+            // ที่เป็นพิกัดบนเส้นโค้ง X และ Y
+            val pubKeyRecovered = publicKey.getDecompress()!!
+
+            // ตรวจสอบลายเซ็นด้วย `ECDSA`
+            val verify = ECDSA.VerifySignature(
+                pubKeyRecovered,
+                rawPermit,
+                signatureRecovered
+            )
+
+            if (verify) {
                 // ตอบกลับด้วยข้อมูลลูกค้าถ้าพบ
                 call.respond(HttpStatusCode.OK, customer)
             } else {
-                // ถ้าไม่พบข้อมูลลูกค้าให้ตอบกลับด้วยสถานะ 404 Not Found
+                // ถ้า `verify` เป็นเท็จ ให้ตอบกลับด้วยสถานะ `400 Bad Request`
                 call.respond(
-                    HttpStatusCode.NotFound,
-                    "Customer not found for ID: $id"
+                    HttpStatusCode.BadRequest,
+                    "ปฏิเสธการให้ข้อมูล เนื่องจากลายเซ็นไม่ถูกต้อง"
                 )
             }
-        } else {
-            // ถ้า ID ไม่ถูกต้องหรือไม่ได้ระบุ ให้ตอบกลับด้วยสถานะ 400 Bad Request
+        } catch (e: IllegalArgumentException) {
+            // ตอบกลับด้วยสถานะ `400 Bad Request` ในกรณีที่มีข้อผิดพลาดเกี่ยวกับข้อมูลขาเข้าไม่ถูกต้อง
             call.respond(
                 HttpStatusCode.BadRequest,
-                "Invalid ID format"
+                e.localizedMessage
+            )
+        } catch (e: NotFoundException) {
+            // ตอบกลับด้วยสถานะ `404 Not Found` ในกรณีที่ไม่พบข้อมูลลูกค้า
+            call.respond(
+                HttpStatusCode.NotFound,
+                e.localizedMessage
+            )
+        } catch (e: Exception) {
+            // ตอบกลับด้วยสถานะ `500 Internal Server Error` ในกรณีที่เกิดข้อผิดพลาดอื่น ๆ
+            call.respond(
+                HttpStatusCode.InternalServerError,
+                "เกิดข้อผิดพลาด: ${e.localizedMessage}"
             )
         }
     }
-
 }
--- src/main/kotlin/org/camrent/routes/api/customer/get/CustomerGetByUserName.kt
+++ src/main/kotlin/org/camrent/routes/api/customer/get/CustomerGetByUserName.kt
@@ -2,6 +2,7 @@ package org.camrent.routes.api.customer.get
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.camrent.database.service.CustomerService
@@ -16,29 +17,35 @@ import org.camrent.utils.ShiftTo.ByteArrayToBigInteger
 fun Route.CustomerByUserName() {
 
     get("customers/name/{name}") {
+        try {
+            // ดึงค่า `Customer name` จากพารามิเตอร์ที่ระบุใน URL และแปลงเป็น String หรือ null ถ้าไม่สามารถแปลงได้
+            val accountName = call.parameters["name"]
 
-        // ดึงค่า `Customer name` จากพารามิเตอร์ที่ระบุใน URL และแปลงเป็น String หรือ null ถ้าไม่สามารถแปลงได้
-        val accountName = call.parameters["name"]
+            // ตรวจสอบว่า `Customer name` ไม่เป็น `null`
+            if (accountName != null) {
+                // ค้นหาข้อมูลลูกค้าด้วย `User Name`
+                val customer = CustomerService.findCustomerByUserName(accountName) ?: throw NotFoundException("ไม่พบลูกค้าสำหรับ `Account Name`: $accountName")
 
-        if (accountName != null) {
-            // ค้นหาข้อมูลลูกค้าด้วย `User Name`
-            val customer = CustomerService.findCustomerByUserName(accountName)
+                // ดึงค่า Witness และ Signature จาก Headers
+                val signature = call.request.headers["Signature"]
+                val witness = call.request.headers["Witness"]
 
-            // ดึงค่า Witness และ Signature จาก Headers
-            val signature = call.request.headers["Signature"]
-            val witness = call.request.headers["Witness"]
+                // ตรวจสอบว่า `Signature` และ `Witness` ไม่เป็น `null`
+                if (signature != null && witness != null) {
+                    // คำนวณ Hash ของ Witness และเปลี่ยนให้เป็นตัวเลข BigInteger
+                    val rawPermit = Sha256.hash(witness.toByteArray()).ByteArrayToBigInteger()
 
-            // ตรวจสอบว่า `Signature` และ `Witness` ไม่เป็น `null`
-            if (signature != null && witness != null) {
-                val rawPermit = Sha256.hash(witness.toByteArray()).ByteArrayToBigInteger()
-
-                if (customer != null) {
                     val signatureRecovered = ECDSA.derRecovered(signature)!!
                     val publicKey = customer.authKey?.B32decode()?.third!!
                     val pubKeyRecovered = publicKey.getDecompress()!!
 
                     // ตรวจสอบลายเซ็นด้วย `ECDSA`
-                    val verify = ECDSA.VerifySignature(pubKeyRecovered, rawPermit, signatureRecovered)
+                    val verify = ECDSA.VerifySignature(
+                        pubKeyRecovered,
+                        rawPermit,
+                        signatureRecovered
+                    )
+
                     if (verify) {
                         // ตอบกลับด้วยข้อมูลลูกค้าถ้าพบ
                         call.respond(HttpStatusCode.OK, customer)
@@ -50,26 +57,37 @@ fun Route.CustomerByUserName() {
                         )
                     }
                 } else {
-                    // ถ้าไม่พบข้อมูลลูกค้าให้ตอบกลับด้วยสถานะ `404 Not Found`
+                    // ถ้า `Signature` หรือ `Witness` เป็น `null` ให้ตอบกลับด้วยสถานะ `400 Bad Request`
                     call.respond(
-                        HttpStatusCode.NotFound,
-                        "ไม่พบลูกค้าสำหรับ `ID`: $accountName"
+                        HttpStatusCode.BadRequest,
+                        "ไม่พบข้อมูล `Signature` หรือ `Witness`"
                     )
                 }
             } else {
-                // ถ้า `Signature` หรือ `Witness` เป็น `null` ให้ตอบกลับด้วยสถานะ `400 Bad Request`
+                // ถ้า `Customer name` ไม่ถูกต้องหรือไม่ได้ระบุ ให้ตอบกลับด้วยสถานะ `400 Bad Request`
                 call.respond(
                     HttpStatusCode.BadRequest,
-                    "ไม่พบข้อมูล `Signature` หรือ `Witness`"
+                    "รูปแบบข้อมูลไม่ถูกต้อง"
                 )
             }
-        } else {
-            // ถ้า `Customer name` ไม่ถูกต้องหรือไม่ได้ระบุ ให้ตอบกลับด้วยสถานะ `400 Bad Request`
+        }  catch (e: IllegalArgumentException) {
+            // ตอบกลับด้วยสถานะ `400 Bad Request` ในกรณีที่มีข้อผิดพลาดเกี่ยวกับข้อมูลขาเข้าไม่ถูกต้อง
             call.respond(
                 HttpStatusCode.BadRequest,
-                "รูปแบบ `ID` ไม่ถูกต้อง"
+                e.localizedMessage
+            )
+        } catch (e: NotFoundException) {
+            // ตอบกลับด้วยสถานะ `404 Not Found` ในกรณีที่ไม่พบข้อมูลลูกค้า
+            call.respond(
+                HttpStatusCode.NotFound,
+                e.localizedMessage
+            )
+        } catch (e: Exception) {
+            // ตอบกลับด้วยสถานะ `500 Internal Server Error` ในกรณีที่เกิดข้อผิดพลาดอื่น ๆ
+            call.respond(
+                HttpStatusCode.InternalServerError,
+                "เกิดข้อผิดพลาด: ${e.localizedMessage}"
             )
         }
     }
-
 }
