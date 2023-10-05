dbab27876558ba57b5b43e3d892d3a13a57b9058
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-10-05 20:48:56 +0700

    ทดสอบการ uploadImage

--- src/main/kotlin/org/camrent/database/test/UploadImage.kt
+++ src/main/kotlin/org/camrent/database/test/UploadImage.kt
@@ -8,46 +8,57 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
 
-
 fun Route.uploadImage() {
     post("image/upload") {
-        val multipart = call.receiveMultipart()
-        var imageFile: File? = null
-        var belief: String? = null
-        var surname: String? = null
+        try {
+            val multipart = call.receiveMultipart()  // รับข้อมูลจาก multipart request
 
-        multipart.forEachPart { part ->
-            when (part) {
-                is PartData.FileItem -> {
-                    imageFile = saveImage(part)
-                }
-                is PartData.FormItem -> {
-                    when (part.name) {
-                        "belief" -> belief = part.value
-                        "surname" -> surname = part.value
+            // ดึงค่า Witness และ Signature จาก Headers
+            val signature = call.request.headers["Signature"]
+            val witness = call.request.headers["Witness"]
+
+            var imageFile: File? = null
+
+            multipart.forEachPart { part ->
+                when (part) {
+                    is PartData.FileItem -> {
+                        imageFile = saveImage(part)  // บันทึกไฟล์ภาพลงในระบบ
                     }
-                }
-                is PartData.BinaryChannelItem, is PartData.BinaryItem -> TODO()
-            }
-        }
 
-        if (imageFile != null) {
-            call.respond(HttpStatusCode.OK, "File uploaded successfully")
-            println("Received image: ${imageFile?.absolutePath}")
-            println("Received belief: $belief")
-            println("Received surname: $surname")
-        } else {
-            call.respond(HttpStatusCode.BadRequest, "No file uploaded")
+                    is PartData.BinaryChannelItem, is PartData.BinaryItem -> {
+                        call.respond(HttpStatusCode.UnsupportedMediaType, "ไม่รองรับข้อมูล Binary")
+                    }
+
+                    is PartData.FormItem -> TODO()
+                }
+            }
+
+            if (imageFile != null) {
+                call.respond(HttpStatusCode.OK, "อัปโหลดไฟล์เรียบร้อยแล้ว")
+                println("ได้รับภาพ: ${imageFile?.absolutePath}")  // พิมพ์ที่อยู่ของไฟล์ภาพที่ถูกอัปโหลด
+            } else {
+                call.respond(HttpStatusCode.BadRequest, "ไม่มีการอัปโหลดไฟล์")  // ตอบกลับว่าไม่มีไฟล์ถูกอัปโหลด
+            }
+
+
+        } catch (e: Exception) {
+            call.respond(
+                HttpStatusCode.InternalServerError,
+                "เกิดข้อผิดพลาด: ${e.message}"
+            )  // ตอบกลับว่าเกิดข้อผิดพลาด
         }
     }
 }
 
 private fun saveImage(imagePart: PartData.FileItem): File {
-    val imageFile = File("src/main/kotlin/org/camrent/database/image/camera/${imagePart.originalFileName}")
+    // สร้างไฟล์ภาพใหม่ตามชื่อเดิม
+    val imageFile = File("src/main/resources/images/account/customers/usr_1/profileImage/${imagePart.originalFileName}")
+    // ใช้ข้อมูลจาก stream provider เพื่อบันทึกไฟล์
     imagePart.streamProvider().use { input ->
         imageFile.outputStream().buffered().use { output ->
             input.copyTo(output)
         }
     }
+    // ส่งคืนไฟล์ภาพที่ถูกบันทึก
     return imageFile
 }
