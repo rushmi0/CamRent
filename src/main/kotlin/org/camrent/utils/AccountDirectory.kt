4a1393f10d2d675cf7ac8100eec66a02da23190c
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-10-05 20:47:01 +0700

    เพิ่ม Method deleteFilesInPathAndCheckExistence เพื่อใช้ในการ ลบไฟล์

--- src/main/kotlin/org/camrent/utils/AccountDirectory.kt
+++ src/main/kotlin/org/camrent/utils/AccountDirectory.kt
@@ -1,5 +1,6 @@
 package org.camrent.utils
 
+import io.ktor.http.content.*
 import java.io.File
 
 object AccountDirectory {
@@ -24,7 +25,7 @@ object AccountDirectory {
             if (!file.exists()) {
                 val created = file.mkdirs()
                 if (created) {
-                    println("Directory '$directoryID' ถูกสร้างเรียบร้อยที่ '$typeUser'.")
+                    println("ไดเร็กทอรี ไอดี '$directoryID' ถูกสร้างเรียบร้อยที่ '$typeUser'.")
                     this.getDirectoryPath(directoryPath)
                     true
                 } else {
@@ -50,16 +51,87 @@ object AccountDirectory {
             val file = File(fullPath)
             if (file.exists() && file.isDirectory) {
                 file.deleteRecursively()
-                println("Directory '$directoryID' deleted successfully at '$typeUser'.")
+                println("ลบไดเร็กทอรี '$directoryID' ที่ '$typeUser' สำเร็จแล้ว")
                 true // ลบ Directory สำเร็จ
             } else {
-                println("Directory '$directoryID' does not exist at '$typeUser'.")
+                println("ไม่มีไดเรกทอรี '$directoryID' ที่ '$typeUser'")
                 false // Directory ไม่มีอยู่
             }
         } catch (e: Exception) {
-            println("An error occurred: ${e.message}")
+            println("เกิดข้อผิดพลาด: ${e.message}")
             false // เกิดข้อผิดพลาด
         }
     }
 
+
+    fun saveImage(id: Int, typeAccount: String, typeImage: String, imagePart: PartData.FileItem): File {
+        val fileName = imagePart.originalFileName ?: "image.jpg"
+
+        val filePath = when {
+            typeAccount == "customers" && typeImage == "profileImage" -> {
+                // src/main/resources/images/account/customers/usr_1/profileImage
+                File("src/main/resources/images/account/$typeAccount/usr_$id/$typeImage/$fileName")
+            }
+
+            typeAccount == "stores" && (typeImage == "profileImage" || typeImage == "camera" || typeImage == "accessories") -> {
+                File("src/main/resources/images/account/$typeAccount/usr_$id/products/$typeImage/$fileName")
+            }
+
+            else -> {
+                throw IllegalArgumentException("ประเภทบัญชีหรือประเภทรูปภาพไม่ถูกต้อง")
+            }
+        }
+
+        // ใช้ข้อมูลจาก stream provider เพื่อบันทึกไฟล์
+        imagePart.streamProvider().use { input ->
+            filePath.outputStream().buffered().use { output ->
+                input.copyTo(output)
+            }
+        }
+        // ส่งคืนไฟล์ภาพที่ถูกบันทึก
+        return filePath
+    }
+
+
+
+    fun deleteFilesInPathAndCheckExistence(filePath: String): Boolean {
+        // สร้างอ็อบเจกต์ของ File จากที่กำหนดให้
+        val fileToDelete = File(filePath)
+
+        return try {
+            // ตรวจสอบว่าไฟล์หรือเส้นทางที่ระบุมีอยู่หรือไม่
+            if (!fileToDelete.exists() || !fileToDelete.isFile) {
+                throw IllegalArgumentException("เส้นทางไฟล์ที่ระบุไม่ถูกต้องหรือไม่มีอยู่: $filePath")
+            }
+
+            // ทำการลบไฟล์
+            val deleted = fileToDelete.delete()
+
+            // ตรวจสอบผลการลบไฟล์และแสดงผลลัพธ์
+            if (deleted) {
+                println("ไฟล์ $filePath ถูกลบเรียบร้อยแล้ว.")
+            } else {
+                println("ไม่สามารถลบไฟล์: $filePath")
+            }
+
+            // ส่งค่าผลลัพธ์การลบไฟล์ออกไป
+            deleted
+        } catch (e: IllegalArgumentException) {
+            // จัดการข้อผิดพลาดที่เกิดจากข้อมูลที่ไม่ถูกต้อง
+            println("ข้อยกเว้น IllegalArgumentException: ${e.message}")
+            false
+        } catch (e: SecurityException) {
+            // จัดการข้อผิดพลาดที่เกิดจากปัญหาด้านความปลอดภัย
+            println("ข้อยกเว้น SecurityException: ${e.message}")
+            false
+        } catch (e: Exception) {
+            // จัดการข้อผิดพลาดที่ไม่ได้ระบุไว้ล่วงหน้า
+            println("เกิดข้อผิดพลาดที่ไม่คาดคิด: ${e.message}")
+            false
+        }
+    }
+
+
 }
+
+
