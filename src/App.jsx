929308301fd0cf118bbf46be200c476d103f0129
CJ1426 <japanchaiyapat@gmail.com>
2023-10-16 22:29:59 +0700

    add reportstore text

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import RootLayout from "./components/RootLayout";
 import Home from "./page/Home";
 import Shop from "./components/shop/Shop";
+import ReportStore from "./components/operationcontrol/storecomment/ReportStore";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           {/* RootLayout contain navigation bar as a component to use in everypage*/}
           <Route element={<RootLayout />}>
+            <Route path="/test" element={ <ReportStore/> }></Route>
             {/* every elements inside RootLayout will show Navigation Bar on the top of the page */}
           </Route>
           {/* RootLayout-end */}
--- src/components/operationcontrol/storecomment/ReportStore.jsx
+++ src/components/operationcontrol/storecomment/ReportStore.jsx
@@ -8,22 +8,7 @@ function ReportStore(porps) {
         <div className="grid grid-cols-2 p-2 rounded bg-gray-200 text-gray-900">
           <ul className="col-span-2 menu menu-vertical rounded text-center border border-gray-300 mb-1 p-0 grid divide-y-2 divide-gray-300">
             <button onClick={() => {}}>
-              <li className="text-sm hover:bg-gray-300 p-1">test</li>
-            </button>
-            <button onClick={() => {}}>
-              <li className="text-sm hover:bg-gray-300 p-1">test</li>
-            </button>
-            <button onClick={() => {}}>
-              <li className="text-sm hover:bg-gray-300 p-1">test</li>
-            </button>
-            <button onClick={() => {}}>
-              <li className="text-sm hover:bg-gray-300 p-1">test</li>
-            </button>
-            <button onClick={() => {}}>
-              <li className="text-sm hover:bg-gray-300 p-1">test</li>
-            </button>
-            <button onClick={() => {}}>
-              <li className="text-sm hover:bg-gray-300 p-1">test</li>
+              <li className="text-sm hover:bg-gray-300 p-1">ได้สินค้าไม่ตรงตามที่สั่ง</li>
             </button>
           </ul>
           <button className="btn btn-error btn-sm" onClick={() => close()}>
