96566981428c09b43d5df6a940624240a0745c82
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-10-17 05:22:49 +0700

    update server

--- src/main/kotlin/org/camrent/routes/api/stores/StoresAPI.kt
+++ src/main/kotlin/org/camrent/routes/api/stores/StoresAPI.kt
@@ -1,8 +1,24 @@
 package org.camrent.routes.api.stores
 
-
 import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import org.camrent.routes.api.stores.get.StoreGet
+import org.camrent.routes.api.stores.get.StoreGetImageURL
 
 fun Application.storesRoute() {
+
+    routing {
+
+        route("api/v1") {
+
+            // `GET` "/stores" : ใช้เพื่อเรียกดูข้อมูลจาก ตาราง `Stores` ทั้งหมด
+            StoreGet()
+
+            // `GET` "/stores/img/id/{id}/{image}" : ใช้สำหรับ ดึงรูป `Profile` ของ `Store`
+            StoreGetImageURL()
+
+        }
+
+    }
 
 }
---
+++ src/main/kotlin/org/camrent/routes/api/stores/get/StoreGet.kt
@@ -0,0 +1,11 @@
+package org.camrent.routes.api.stores.get
+
+import io.ktor.server.routing.*
+
+fun Route.StoreGet() {
+
+    get("stores") {
+
+    }
+
+}
